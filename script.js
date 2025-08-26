379da60f87b51df7c710579a4f0775027b9037a5
serpenterius <nellishiro@gmail.com>
2025-08-26 13:38:53 +0300

    Almost made logic

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-
+//Сделать правильную передачу количества блюд в коризну (СДЕЛАНО)
+//Сделать правильную логику, при добавлени одного и того же блюда (СДЕЛАНО)
+//Сделать правильное отображение цены в total (СДЕЛАНО)
+//Сделать правильное отображение количества позиций в корзине (СДЕЛАНО)
+//Доделать стили
+//Сделать корректный адаптив
 
 
 
@@ -8,6 +13,7 @@ const menu = document.querySelector('.menu-items'),
     cart = document.querySelector('.cart-items')
 
 let cartData = [];
+let totalPrice = 0;
 
 //Получаю данные из файла
 function init() {
@@ -29,6 +35,7 @@ function init() {
 function render(data) {
     menu.innerHTML = '';
     cart.innerHTML = '';
+    document.querySelector('.cart-positions').textContent = `Your cart(${cartData.length})`;
 
     //Динамический рендер элементов меню
     data.forEach(item => {
@@ -40,7 +47,8 @@ function render(data) {
                 <img class='menu-dish' src="${image.desktop}" alt="">
                 <button class="add-to-cart">
                     <div class="default-btn">
-                        Add to cart
+                        <img src="assets/images/icon-add-to-cart.svg" alt="cart">
+                        Add to Cart
                     </div>
                     <div class="hover-btn hidden">
                         <img class="decrement" src="assets/images/icon-decrement-quantity.svg" alt="">
@@ -62,16 +70,16 @@ function render(data) {
 
     //Динамический рендер элементов корзины
     cartData.forEach((item, index) => {
-        const {name, price} = item;
+        const {name, price, quantity} = item;
 
         cart.insertAdjacentHTML('beforeend', `
             <div class="cart-item" data-id=${index}>
                 <p>${name}</p>
 
                 <div class="cart-item-info">
-                    <p class="quantity">1x</p>
+                    <p class="quantity">${quantity}x</p>
                     <p class="price">${price}</p>
-                    <p class="price-all">${price}</p>
+                    <p class="price-all">$${Number(price.slice(1)) * Number(quantity)}</p>
                 </div>
             
                 <span class="cart-remove">
@@ -81,29 +89,48 @@ function render(data) {
         `)
     })
 
+    //Подсчёт суммы позиций в корзине и её вывод пользователю
+    totalPrice = 0;
+    cartData.forEach(item => {
+        totalPrice = totalPrice + Number(item.price.slice(1)) * Number(item.quantity);
+    })
+    document.querySelector('.order-price').textContent = `$${totalPrice}`;
+
 
     //Удаление позиции из корзины покупок
     document.querySelectorAll('.cart-remove').forEach(item => {
         item.addEventListener('click', e => {
             e.currentTarget.closest('.cart-item').remove();
             cartData.splice(e.currentTarget.closest('.cart-item').dataset.id, 1);
-            console.log(cartData);
+            document.querySelector('.cart-positions').textContent = `Your cart(${cartData.length})`;
+            
+            init();
         })
     })
 
     //Добавление элемента из меню в корзину покупок
     document.querySelectorAll('.add-to-cart').forEach(item => {
         item.addEventListener('click', e => {
-            // e.stopPropagation();
-
+            let hasTwin = false;
             let object = {
                 name:e.currentTarget.closest('.menu-item').children[1].children[1].textContent,
-                price: e.currentTarget.closest('.menu-item').children[1].children[2].textContent
+                price: e.currentTarget.closest('.menu-item').children[1].children[2].textContent,
+                quantity: e.currentTarget.closest('.menu-item').querySelector('.quantity-btn').textContent
             }
 
+            cartData.forEach(item => {
+                if(item.name === object.name) {
+                    // console.log(`Вы пытаетесь добавить уже существующую позицию ${object.name}, количество в корзине: ${item.quantity}. Количество добавляемого: ${object.quantity}`);
+                    item.quantity = Number(item.quantity) + Number(object.quantity);
+                    hasTwin = true;
+                } else {
+                    hasTwin = false;
+                }
+            })
 
-            cartData.push(object);
-            // console.log(cartData);
+            if(!hasTwin) {
+                cartData.push(object);
+            }
 
             init();
         })
